22e6e7d8f3957ef8e8a92b2f6c313b5de1f2eb4c
ehsan1814 <mr.ehsan.kaji@gmail.com>
2020-01-22 13:14:54 +0330

    user make group completed

--- Main/src/main/java/model/Teacher.java
+++ Main/src/main/java/model/Teacher.java
@@ -2,6 +2,7 @@ package model;
 
 import controller.Const;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -47,6 +48,7 @@ public class Teacher implements Role,FormWriter{
 	    this.forms.add(new Form(formsArray.getString(i)));
     }
   }
+
 
   public void addForm(Form form){
 
@@ -132,4 +134,42 @@ public class Teacher implements Role,FormWriter{
   public String get_id() {
     return this._id;
   }
+
+
+  public static void userMakeGroup(MongoClient client, User user, JsonObject clientJson, Handler<AsyncResult<String>> handler) {
+    JsonObject workshop = new JsonObject()
+      .put("_id" , clientJson.getString("workshopId"));
+
+    JsonArray f = new JsonArray()
+      .add(new JsonArray().add("1").add("2").add("3").add("4").add("5"));
+    f.add(new JsonArray().add("11").add("12").add("13").add("14"));
+    f.add(new JsonArray().add("21").add("22").add("23").add("24"));
+    f.add(new JsonArray().add("31").add("32").add("33").add("34"));
+    System.out.println(f.getJsonArray(0).size());
+
+    JsonArray groups = clientJson.getJsonArray("groups");
+    JsonArray mainGroups = new JsonArray();
+    for (int k = 0 ; k < groups.size() ; k++){
+      mainGroups.add(groups.getJsonArray(k));
+    }
+
+    client.find(Const.enteredCourse , workshop , resFindWorkshop ->{
+      if (resFindWorkshop.succeeded() && !resFindWorkshop.result().isEmpty()){//we find workshop in db
+        EnteredCourse findedWorkshop = new EnteredCourse(resFindWorkshop.result().get(0));
+        for (int i = 0 ; i < mainGroups.size() ; i++) {
+          Group g1 = new Group();
+          for (int j = 0; j < mainGroups.getJsonArray(i).size() ; j++) {//condition for loop is size of the each group
+            g1.addIdentity(mainGroups.getJsonArray(i).getString(j));
+          }
+          g1.saveToDB(client);
+          findedWorkshop.addGroup(g1);
+        }
+        findedWorkshop.update(client);
+      }
+      else {//if we don't find workshop in db
+        handler.handle(Future.failedFuture("Workshop doesn't exist."));
+      }
+    });
+	}
+
 }
