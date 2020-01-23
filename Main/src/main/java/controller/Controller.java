3a06bb0fb573f3c67e14d101355c7ee39ba627f7
mersad <mersadkhalafi@gmail.com>
2020-01-24 03:23:50 +0330

    add collection name in findIn function

--- Main/src/main/java/controller/Controller.java
+++ Main/src/main/java/controller/Controller.java
@@ -13,12 +13,12 @@ public class Controller {
 
 
 
-  public static void findIn(MongoClient client, String fieldName, String in, Handler<AsyncResult<List<JsonObject>>>handler){
+  public static void findIn(MongoClient client,String collection, String fieldName, String in, Handler<AsyncResult<List<JsonObject>>>handler){
 
     JsonObject query = new JsonObject()
       .put(fieldName,new JsonObject().put("$in",new JsonArray().add(in)));
 
-    client.find(Const.user,query,handler);
+    client.find(collection,query,handler);
 
   }
 
